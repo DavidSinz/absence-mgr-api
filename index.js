9932eddbd6be04ab52c8e292de082be1dbc20790
DavidSinz <davidmarksinz@gmail.com>
2022-02-22 14:41:20 +0100

    allowing cross origin access

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const absencesData = require("./absences_data/absences.json");
 const membersData = require("./absences_data/members.json");
 let port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.get("/", (req, res) => {
   const data = absencesData.map((a) => ({
     ...a,

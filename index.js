b00d8ff38581e3baba7756a9b1f60e08af18c729
DavidSinz <davidmarksinz@gmail.com>
2022-02-22 13:12:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const absencesData = require("./absences_data/absences.json");
+const membersData = require("./absences_data/members.json");
+let port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("");
+});
+
+app.get("/absence-manager-api", (req, res) => {
+  const data = absencesData.map((a) => ({ ...a, ...membersData.find((b) => b.userId === a.userId) }));
+
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log(
+    `Absence Manager API is listening on port http://localhost:${port}`
+  );
+});
